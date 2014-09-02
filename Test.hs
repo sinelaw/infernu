96d6eefb989d224d5143d7130516bca2064ccb23
Noam Lewis <jones.noamle@gmail.com>
2014-09-03 01:43:42 +0300

    compiles

--- Test.hs
+++ Test.hs
@@ -3,6 +3,8 @@ module Test where
 import Data.Either(isLeft, lefts)
 
 data Type = Unknown | JNumber | JString | JRegex | JArray Type | JObject [(String, Type)] | JFunc [Type] Type
+          deriving (Show, Eq)
+
 data Expr = LitNumber Double | LitString String | LitRegex String | LitArray [Expr] | LitObject [(String, Expr)]
 	  | Var String -- TODO: perhaps there should only be Property (and for function scopes use a hidden base obj?)
           | Plus Expr Expr | Minus Expr Expr 
@@ -12,21 +14,26 @@ data Expr = LitNumber Double | LitString String | LitRegex String | LitArray [Ex
           deriving (Show, Eq)
 
 data TypeError = TypeError String Expr [TypeError]
+               deriving (Show, Eq)
 
 inferType :: Expr -> Either TypeError Type
-inferType (LitNumber x) = Right JNumber
-inferType (LitString x) = Right JString
-inferType (LitRegex x) = Right JRegex
+inferType (LitNumber _) = Right JNumber
+inferType (LitString _) = Right JString
+inferType (LitRegex _) = Right JRegex
 inferType (LitArray []) = Right $ JArray Unknown
-inferType ar@(LitArray (x:xs)) = if areSameType 
-                                 then Right $ JArray headType
-                                 else Left "array elements are of inconsistent type" ar
+inferType ar@(LitArray (x:xs))
+    | areSameType = fmap JArray headType
+    | otherwise = Left $ TypeError "array elements are of inconsistent type" ar []
     where headType = inferType x
-          areSameType = all (\y -> y == x) $ map inferType xs
+          areSameType = all (headType ==) $ map inferType xs
 
-inferType obj@(LitObject xs) = if (any isLeft propTypes) 
-                               then Left $ TypeError "properties are badly typed" obj (lefts propTypes) 
-                               else Right $ JObject propNamedTypes
-    where propNamedTypes = map (\name expr -> (name, inferType expr)) xs
+inferType obj@(LitObject xs) 
+    | any isLeft propTypes = Left $ TypeError "properties are badly typed" obj (lefts propTypes) 
+    | otherwise = Right . JObject $ map (\(name, expr) -> (name, fromRight expr)) propNamedTypes
+    where propNamedTypes = map (\(name, expr) -> (name, inferType expr)) xs
           propTypes = map snd propNamedTypes
-inferType expr = Left "not implemented" expr
+inferType expr = Left $ TypeError "not implemented" expr []
+
+fromRight :: Either a b -> b
+fromRight (Right x) = x
+fromRight _ = error "Expected Right" 
