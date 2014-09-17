eb99d37fda269f39c70d78e7fadf06c95112e617
Noam Lewis <jones.noamle@gmail.com>
2014-09-17 23:31:36 +0300

    add test flie

---
+++ test/valid/promise.js
@@ -0,0 +1,22 @@
+
+
+
+function once(f) {
+    var ran = true;
+    var result;
+    return function(x) {
+	if (ran) {
+	    ran = false;
+	    result = f(x);
+	}
+	return result;
+    };
+}
+
+//var g = once(function(x) { return x; })
+
+var h = once(function(x) { return 'shimon'; });
+//var j = g('moshe');
+
+//g(2);
+//h(false);
