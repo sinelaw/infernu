c1983eeb6a3b10ec2a29b0bd7c44f728e55c01a8
Noam Lewis <jones.noamle@gmail.com>
2014-10-01 20:40:36 +0300

    add test for ST-style get/put

---
+++ test/invalid/getput.js
@@ -0,0 +1,16 @@
+function() {
+    function getput() {
+	var x;
+	return {
+	    get: function() { return x; },
+	    put: function(y) { var old = x; x = y; return old; }
+	};
+    };
+
+    var gp = getput();
+
+    //gp.put(2);
+    //var num = gp.get();
+
+    //gp.put('a'); // should fail!
+}
