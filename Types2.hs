49e97eea60c54411ae206862cf16d287552f2cb6
Noam Lewis <jones.noamle@gmail.com>
2014-11-04 20:53:54 +0200

    (WIP) add some tests to Types2

--- Types2.hs
+++ Types2.hs
@@ -2,28 +2,24 @@
 module Types2 where
 
 import Data.Functor((<$>))
-import Control.Monad.State(State, get, modify)
+import Control.Monad.State(State, runState, get, modify)
 import qualified Data.Map.Lazy as Map
 import Control.Monad(forM)
-
-----------------------------------------------------------------------
-
-(|-) :: Maybe a -> a -> a
-(Just x) |- _ = x
-_ |- y = y
+import Data.Maybe(fromMaybe)
 
 ----------------------------------------------------------------------
 
 type VarName = String
 
 data LitVal = LitNumber Double | LitBoolean Bool | LitString String
+            deriving (Show, Eq)
  
 data Exp = Var VarName
          | App Exp Exp
          | Lam VarName Exp
          | Let VarName Exp Exp
          | Lit LitVal
-
+         deriving (Show, Eq)
 ----------------------------------------------------------------------
 
 type TVarName = Int
@@ -31,6 +27,7 @@ type TVarName = Int
  
 data TBody = TVar TVarName
             | TNumber | TBoolean | TString 
+            deriving (Show, Eq)
 
 data Type t = TBody t
             | TFunc (Type t) (Type t)
@@ -38,26 +35,42 @@ data Type t = TBody t
 
 ----------------------------------------------------------------------
 
+-- | Type scheme: a type expression with a "forall" over some type variables that may appear in it (universal quantification).
 data TScheme = TQual [TVarName] (Type TBody)
+             deriving (Show, Eq)
 
+-- Used internally to generate fresh type variable names
 data NameSource = NameSource { lastName :: TVarName }
+                deriving (Show, Eq)
+
+-- | Inference monad. Used as a stateful context for generating fresh type variable names.
 type Infer a = State NameSource a
+
+runInfer :: Infer a -> a
+runInfer inf = fst . runState inf $ (NameSource { lastName = 0 })
 
 fresh :: Infer TVarName
 fresh = do
   modify (\ns -> ns { lastName = lastName ns + 1 })
   lastName <$> get
-  
 
+-- | Instantiate a type scheme by giving fresh names to all quantified type variables.
+--
+-- For example:
+--
+-- >>> runInfer . instantiate $ TQual [0] (TFunc (TBody (TVar 0)) (TBody TNumber))
+-- TFunc (TBody (TVar 1)) (TBody TNumber)
+--
 instantiate :: TScheme -> Infer (Type TBody)
 instantiate (TQual tvarNames t) = do
   allocNames <- forM tvarNames $ \tvName -> do
-                                    freshName <- fresh
-                                    return (tvName, freshName)
-  return $ fmap (\t' -> case t' of
-                          (TVar n) -> TVar $ (lookup n allocNames |- n)
-                          _ -> t') t
+    freshName <- fresh
+    return (tvName, freshName)
 
+  let replaceVar (TVar n) = TVar . fromMaybe n $ lookup n allocNames
+      replaceVar x = x
+
+  return $ fmap replaceVar t
 
 ----------------------------------------------------------------------
 
