de350172550ba64548646127583209a755bb8e3e
Noam Lewis <jones.noamle@gmail.com>
2014-11-25 10:18:48 +0200

    improve formatting of runtest.sh

--- test/runtest.sh
+++ test/runtest.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
 set -u
-../dist/build/sjs-demo/sjs-demo $@ 2>&1 | tail -1 | xargs echo $@
+ARGS="$@"
+../dist/build/sjs-demo/sjs-demo $ARGS 2>&1 | tail -1 | xargs -iresult printf "%-5s %s\n" result "$ARGS"
