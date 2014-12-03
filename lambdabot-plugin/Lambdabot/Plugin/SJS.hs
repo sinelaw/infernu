89d1c4917a0340fee1ba6ad69879dce6e776357d
Noam Lewis <jones.noamle@gmail.com>
2014-12-03 22:10:19 +0200

    add lambdabot plugin

---
+++ lambdabot-plugin/Lambdabot/Plugin/SJS.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE TemplateHaskell, MultiParamTypeClasses, TypeSynonymInstances #-}
+-- | SJS
+module Lambdabot.Plugin.SJS (sjsPlugin) where
+
+import Lambdabot.Plugin
+import Data.Functor ((<$>))
+import qualified Data.Map as Map
+import qualified Data.ByteString.Char8 as P
+import Data.List(intersperse)
+import qualified Language.ECMAScript3.Parser as ES3Parser
+import qualified Language.ECMAScript3.Syntax as ES3
+
+import           SafeJS.Infer       (pretty, runTypeInference, getAnnotations)
+--import           SafeJS.Util        (chec
+import           SafeJS.Parse                (translate)
+
+
+sjsPlugin :: Module ()
+sjsPlugin = newModule
+    { moduleCmds = return
+        [ (command "js")
+            { aliases = []
+            , help = say "js <javascript expression>"
+            , process = say . sayType
+            }
+        ]
+    }
+
+
+--sayType :: Monad m => String -> Cmd m ()
+sayType :: String -> String
+sayType rest = case  runTypeInference . translate . ES3.unJavaScript <$> ES3Parser.parseFromString rest of
+                Left e -> show e
+                Right res -> case getAnnotations <$> res of
+                              Left e' -> show e'
+                              Right [] -> show "There is nothing there."
+                              Right xs -> show . snd $ last xs
+                       
