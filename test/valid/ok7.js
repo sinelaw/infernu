7561f4e5a1c0ba7b0f7c400578cbb9c27ecb21fc
Noam Lewis <jones.noamle@gmail.com>
2014-12-09 11:58:34 +0200

    make test ok7.js a bit more realistic

--- test/valid/ok7.js
+++ test/valid/ok7.js
@@ -1,9 +1,11 @@
 function makeX() {
     var x = [];
     var setX = function (v2) { x = [v2]; return true; };
-    return setX;
+    return { setX: setX, getX: function() { return x; } };
 }
 var mx1 = makeX();
-mx1(false);
+mx1.setX(false);
+var x1 = mx1.getX();
 var mx2 = makeX();
-mx2(3);
+mx2.setX(3);
+var x2 = mx2.getX();
