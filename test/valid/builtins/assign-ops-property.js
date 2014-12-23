e98675dcfc3fdb3c897fe0690eed7650a5266865
Noam Lewis <jones.noamle@gmail.com>
2014-12-23 23:44:43 +0200

    add (valid) tests for assign operators

---
+++ test/valid/builtins/assign-ops-property.js
@@ -0,0 +1,13 @@
+var obj  = { x: 0 };
+obj.x = 1;
+obj.x += 2;
+obj.x -= 3;
+obj.x *= 4;
+obj.x /= 5;
+obj.x %= 8;
+obj.x <<= 6;
+obj.x >>= 7;
+obj.x >>>= 8;
+obj.x &= 9;
+obj.x ^= 10;
+obj.x |= 11;
