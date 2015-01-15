7fb861a53409d68f3040995ce2d4d762694fa7df
Noam Lewis <jones.noamle@gmail.com>
2015-01-15 10:51:42 +0200

    fix wrong test invalid/object-chaining.js

--- test/invalid/object-chaining.js
+++ test/invalid/object-chaining.js
@@ -3,9 +3,5 @@ function Obj(y) {
     this.doIt = function(x) { this.x = x; return this; };
 }
 var o = new Obj(2);
-o.doIt(2);
-o;
-// function wrapDoIt(x) { return o.doIt(x); }
-// var o2 = o.doIt(2);
-// o2;
-//var o3 = o2.doIt('a');
+var o2 = o.doIt(2);
+var o3 = o2.doIt('a');
