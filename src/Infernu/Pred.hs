3bea53c9f295afdf07093d033a6ff325dee99d20
Noam Lewis <jones.noamle@gmail.com>
2015-02-11 00:17:02 +0200

    WIP: canonical predicates

--- src/Infernu/Pred.hs
+++ src/Infernu/Pred.hs
@@ -1,9 +1,9 @@
 module Infernu.Pred
-       (toDNF, toList)
+       (toDNF)
     where 
 
 
-import Infernu.Types (TPred(..))
+import Infernu.Types (TPred(..), TVarName)
 
 data CanonPredOr t = CanonPredOr [CanonPredAnd t]
                      deriving (Show, Eq)
@@ -12,6 +12,7 @@ data CanonPredAnd t = CanonPredAnd [CanonPredEq t]
                       deriving (Show, Eq)
 
 data CanonPredEq t = CanonPredEq TVarName t
+                      deriving (Show, Eq)
 
 -- | Converts a predicate to DNF (Disjunction Normal Form)
 -- based on code from the hatt package
@@ -42,25 +43,26 @@ disj e1 e2 = if e1 == e2
              else e1 `TPredOr` e2
 
 -- | Converts a predicate to a list of sums of products
--- >>> toList $ TPredAnd (TPredOr (TPredEq 0 'a') (TPredEq 1 'b')) (TPredOr (TPredEq 0 'c') (TPredEq 1 'd'))
--- [[TPredAnd (TPredEq 0 'a') (TPredEq 0 'c')],[TPredAnd (TPredEq 0 'a') (TPredEq 1 'd')],[TPredAnd (TPredEq 1 'b') (TPredEq 0 'c')],[TPredAnd (TPredEq 1 'b') (TPredEq 1 'd')]]
-toList :: Eq t => TPred t -> [[TPred t]]                                      
-toList = toList' . toDNF
-    where toList' p@(TPredAnd _ _) = [[p]]
-          toList' (TPredOr p1 p2) = toList' p1 ++ toList' p2
-          toList' p = [[TPredAnd p TPredNothing]]
-          
+toCanon :: Eq t => TPred t -> CanonPredOr t
+toCanon = CanonPredOr . toCanonOr' . toDNF
+    where toCanonOr' (TPredOr p1 p2) = toCanonOr' p1 ++ toCanonOr' p2
+          toCanonOr' (TPredAnd p1 p2) = [CanonPredAnd $ (toCanonAnd' p1) ++ (toCanonAnd' p2)]
+          toCanonOr' p = [CanonPredAnd $ toCanonAnd' p]
 
+          toCanonAnd' (TPredOr _ _) = error "toDNF didn't supply DNF..."
+          toCanonAnd' (TPredAnd p1 p2) = (toCanonAnd' p1) ++ (toCanonAnd' p2)
+          toCanonAnd' (TPredEq v t) = [CanonPredEq v t]
+          toCanonAnd' TPredNothing = []
 
-fromList :: [[TPred t]] -> TPred t
-fromList [] = TPredNothing
-fromList [[]] = TPredNothing
-fromList [x:xs] = TPredOr x (fromListAnd xs)
-    where fromListAnd [] = TPredNothing
-          fromListAnd (x:xs) = TPredAnd x (fromListAnd xs)
+fromCanon :: CanonPredOr t -> TPred t
+fromCanon (CanonPredOr []) = TPredNothing
+fromCanon (CanonPredOr (p:ps)) = foldr TPredOr (fromCanonAnd p) $ map fromCanonAnd ps
+    where fromCanonAnd (CanonPredAnd []) = TPredNothing
+          fromCanonAnd (CanonPredAnd (p:ps)) = foldr TPredAnd (fromCanonEq p) (map fromCanonEq ps)
+          fromCanonEq (CanonPredEq v t) = TPredEq v t
     
-unifyPreds :: Eq t => (t -> t -> Bool) -> TPred t -> TPred t -> Maybe (Pred t)
-unifyPreds uni p1 p2 = unifyPreds' (toList p1) (toList p2)
-    where unifyPreds' [] p2' = Right $ fromList p2'
-          unifyPreds' p1' [] = Right $ fromList p1'
-          unifyPreds' 
+-- unifyPreds :: Eq t => (t -> t -> Bool) -> TPred t -> TPred t -> Maybe (Pred t)
+-- unifyPreds uni p1 p2 = unifyPreds' (toList p1) (toList p2)
+--     where unifyPreds' [] p2' = Right $ fromList p2'
+--           unifyPreds' p1' [] = Right $ fromList p1'
+--           unifyPreds' 
