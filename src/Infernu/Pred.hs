a4b746fcb51de106490bb510b4427cb77756bd52
Noam Lewis <jones.noamle@gmail.com>
2015-02-15 14:16:14 +0200

    WIP - refactor Pred.unify

--- src/Infernu/Pred.hs
+++ src/Infernu/Pred.hs
@@ -1,15 +1,16 @@
 {-# LANGUAGE TupleSections #-}
 module Infernu.Pred
        (unify, mkAnd, mkOr)
-    where 
+    where
 
 
-import Data.Maybe (catMaybes)
-import Infernu.Types (TPred(..), TVarName)
-import qualified Data.Map.Lazy             as Map
-import Data.Map.Lazy (Map)
-import qualified Data.Set                  as Set
-import           Data.Set                  (Set)
+import           Data.Traversable (sequenceA)
+import           Data.Map.Lazy (Map)
+import qualified Data.Map.Lazy as Map
+import           Data.Maybe    (catMaybes)
+import           Data.Set      (Set)
+import qualified Data.Set      as Set
+import           Infernu.Types (TPred (..), TVarName)
 
 data CanonPredOr t = CanonPredOr [Map TVarName (Set t)]
                      deriving (Show, Eq)
@@ -20,7 +21,7 @@ data CanonPredOr t = CanonPredOr [Map TVarName (Set t)]
 -- >>> toDNF $ TPredAnd (TPredEq 0 'a') (TPredEq 1 'b')
 -- TPredAnd (TPredEq 0 'a') (TPredEq 1 'b')
 -- >>> toDNF $ TPredOr (TPredEq 0 'a') (TPredEq 1 'b')
--- TPredOr (TPredEq 0 'a') (TPredEq 1 'b')    
+-- TPredOr (TPredEq 0 'a') (TPredEq 1 'b')
 -- >>> let or' = TPredOr (TPredEq 0 'a') (TPredEq 1 'b')
 -- >>> toDNF $ TPredAnd or' or'
 -- TPredOr (TPredEq 0 'a') (TPredEq 1 'b')
@@ -42,14 +43,14 @@ mkAnd TPredTrue y = y
 mkAnd x TPredTrue = x
 mkAnd x y = if x == y then x else TPredAnd x y
 
-                                 
+
 mkOr :: Eq t => TPred t -> TPred t -> TPred t
 mkOr TPredTrue y = y
 mkOr x TPredTrue = x
 mkOr x y = if x == y then x else TPredOr x y
 
 -- | Converts a predicate to a list of sums of products
---                  
+--
 -- >>> toCanon $ TPredAnd (TPredOr (TPredEq 0 'a') (TPredEq 1 'b')) (TPredOr (TPredEq 0 'c') (TPredEq 1 'd'))
 -- CanonPredOr [fromList [(0,fromList "ac")],fromList [(0,fromList "a"),(1,fromList "d")],fromList [(0,fromList "c"),(1,fromList "b")],fromList [(1,fromList "bd")]]
 toCanon :: (Ord t, Eq t) => TPred t -> CanonPredOr t
@@ -62,7 +63,7 @@ toCanon = CanonPredOr . toCanonOr' . toDNF
           toCanonAnd' (TPredEq v t) = Map.singleton v (Set.singleton t)
           toCanonAnd' TPredTrue = Map.empty
 
--- |          
+-- |
 -- >>> fromCanon $ CanonPredOr [Map.fromList [(0,Set.fromList "ac")],Map.fromList [(0,Set.fromList "a"),(1,Set.fromList "d")],Map.fromList [(0,Set.fromList "c"),(1,Set.fromList "b")],Map.fromList [(1,Set.fromList "bd")]]
 -- TPredOr (TPredAnd (TPredEq 1 'd') (TPredEq 0 'a')) (TPredOr (TPredAnd (TPredEq 1 'b') (TPredEq 0 'c')) (TPredOr (TPredAnd (TPredEq 1 'd') (TPredEq 1 'b')) (TPredAnd (TPredEq 0 'c') (TPredEq 0 'a'))))
 fromCanon :: Eq t => CanonPredOr t -> TPred t
@@ -70,24 +71,42 @@ fromCanon (CanonPredOr []) = TPredTrue
 fromCanon (CanonPredOr (p:ps)) = foldr mkOr (fromCanonAnd p) $ map fromCanonAnd ps
     where fromCanonAnd m = Map.foldrWithKey (\k vs r -> Set.foldr (\v p' -> mkAnd p' $ TPredEq k v) r vs) TPredTrue m
 
--- | Main unification function: checks if two predicates can be combined in a conjunction (i.e. if they can ANDed)          
+-- | Main unification function: checks if two predicates can be combined in a conjunction (i.e. if they can ANDed)
 -- >>> unify (==) (TPredAnd (TPredEq 0 'a') (TPredEq 1 'b')) (TPredEq 0 'a')
 -- Just (TPredAnd (TPredEq 1 'b') (TPredEq 0 'a'))
 -- >>> unify (==) (TPredOr (TPredEq 0 'a') (TPredEq 1 'b')) (TPredEq 0 'a')
 -- Just (TPredOr (TPredAnd (TPredEq 1 'b') (TPredEq 0 'a')) (TPredEq 0 'a'))
-unify :: Ord t => (t -> t -> Bool) -> TPred t -> TPred t -> Maybe (TPred t)
+unify :: (Applicative f, Ord t) => (t -> t -> f ()) -> TPred t -> TPred t -> f (TPred t)
 unify u p1 p2 = fmap fromCanon $ unifyPreds u (toCanon p1) (toCanon p2)
-                
-unifyPreds :: Ord t => (t -> t -> Bool) -> CanonPredOr t -> CanonPredOr t -> Maybe (CanonPredOr t)
+
+unifyPreds :: (Applicative f, Ord t) => (t -> t -> f ()) -> CanonPredOr t -> CanonPredOr t -> f (CanonPredOr t)
 unifyPreds u (CanonPredOr m1s) (CanonPredOr m2s) =
-    case catMaybes [unifyMaps u m1 m2 | m1 <- m1s, m2 <- m2s] of
-        [] -> Nothing
-        ms -> Just $ CanonPredOr ms
-    
-unifyMaps :: Ord t => (t -> t -> Bool) -> Map TVarName (Set t) -> Map TVarName (Set t) -> Maybe (Map TVarName (Set t))
+    CanonPredOr <$> sequenceA [unifyMaps u m1 m2 | m1 <- m1s, m2 <- m2s]
+
+-- TODO: REfactor    
+-- Map a   mismatching keys -> survive the key as is
+--     b   matching keys -> unify the values, fail delete keys
+--     
+-- typevar => Type          typevar => Type 
+--       a => Number   &    a = Int
+--       b => String   &    b = Number
+--       c => Number
+
+-- Get rid of Set - do the unification at the toCanon level
+-- Use newtypes
+-- Use effectful unifyWith (using pure to lift pure values in the map to f a)
+      
+--    (╯°□°）╯︵ ┻━┻
+unifyMaps ::
+    (Applicative f, Ord t, Ord k)
+    => (t -> t -> f ())
+    -> Map k (Set t)
+    -> Map k (Set t)
+    -> f (Map k (Set t))
+unifyMaps u m1 m2 = 
 unifyMaps u m1 m2 = Map.traverseWithKey (\_ (isGood, s) -> if isGood then Just s else Nothing)
                     $ Map.mergeWithKey (\_ t1 t2 -> Just (unifySets u t1 t2, Set.union t1 t2)) (Map.map (True,)) (Map.map (True,)) m1 m2
 
-unifySets :: Ord t => (t -> t -> Bool) -> Set t -> Set t -> Bool
-unifySets u s1 s2 = foldr (\(x,y) p -> p && (x `u` y)) True [(x,y) | x <- Set.toList s1, y <- Set.toList s2]
-    
+unifySets :: (Applicative f, Ord t) => (t -> t -> f ()) -> Set t -> Set t -> f ()
+unifySets u s1 s2 = sequenceA_ [x `u` y | x <- Set.toList s1, y <- Set.toList s2]
+
