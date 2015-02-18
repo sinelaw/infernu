86ea168160c5ed9dc67a733bb5904698fd92abc6
Noam Lewis <jones.noamle@gmail.com>
2015-02-18 22:46:05 +0200

    print less information about recursive types to make signatures that include them sane to read

--- test/valid/point.js
+++ test/valid/point.js
@@ -5,4 +5,17 @@ function rotate(angle, p) {
     p.new_x(cos(angle) * p.get_x() - sin(angle) * p.get_y());
     p.new_y(sin(angle) * p.get_x() + cos(angle) * p.get_y());
 }
-rotate;
+
+function Point(x0, y0) {
+    var x = x0, y = y0;
+    this.new_x = function(x1) { x = x1; };
+    this.new_y = function(y1) { y = y1; };
+    this.get_x = function() { return x; };
+    this.get_y = function() { return y; };
+}
+
+var p = new Point(1,2);
+
+rotate(20, p);
+
+
