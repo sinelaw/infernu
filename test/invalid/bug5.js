79ff40709b540f4393d8363e9a0f757d0c9747a5
Noam Lewis <jones.noamle@gmail.com>
2015-03-13 10:41:26 +0200

    improve docs on test invalid/bug5.js

--- test/invalid/bug5.js
+++ test/invalid/bug5.js
@@ -1,6 +1,6 @@
-var x = function (a) { return a; }; // X = forall a. a -> a
-var getX = function (v1) { return x; }; // forall b. X = forall b X(a). X(a)
-var setX = function (v2) { x = v2; return true; }; // X -> Bool
-setX(function(a2) { return 'a'; }); // X ~ forall c. c -> String  ==>  forall a. a -> a ~ forall c. c -> String  ==>  a=c, a=String  ==>  X = String -> String
+var x = function (a) { return a; };     // forall a. a -> a            | c -> c                    | (String -> String)
+var getX = function (v1) { return x; }; // forall b a. b -> (a -> a)   | forall b. b -> (c -> c)   | forall b. b -> (String -> String)
+var setX = function (v2) { x = v2; return true; }; // x is mutable =>  | (c -> c) -> Bool          | (String -> String) -> Bool
+setX(function(a2) { return 'a'; }); //                                                             | c => String 
 getX(false)(false);
 
