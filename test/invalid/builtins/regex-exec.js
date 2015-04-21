ff8f4d4b27002bc31140c27f994f6bca0bb60fff
Noam Lewis <jones.noamle@gmail.com>
2015-04-21 21:27:31 +0300

    add regex.exec property

---
+++ test/invalid/builtins/regex-exec.js
@@ -0,0 +1,14 @@
+var r = /123/g;
+
+var m = r.exec('12');
+
+
+var x = 0;
+x = m.index;
+
+var s = '';
+s = m[0];
+s = m.input;
+
+m[0] = 'a';
+
---
+++ test/valid/builtins/regex-exec.js
@@ -0,0 +1,12 @@
+var r = /123/g;
+
+var m = r.exec('12');
+
+
+var x = 0;
+x = m.index;
+
+var s = '';
+s = m[0];
+s = m.input;
+
