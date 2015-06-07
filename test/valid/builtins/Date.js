1c3b3b40628e565ef8b5992ada0505af91a24bae
Noam Lewis <jones.noamle@gmail.com>
2015-06-08 00:53:57 +0300

    add tests for some Date methods

--- test/valid/builtins/Date.js
+++ test/valid/builtins/Date.js
@@ -2,3 +2,25 @@ var x = new Date();
 var y = Date.now();
 var z = Date.parse('2015-01-01');
 var u = Date.UTC(2015,3,1,11,10,9,932);
+
+
+var n = 1;
+
+n = x.getDate();
+n = x.getDay();
+n = x.getFullYear();
+n = x.getHours();
+n = x.getMilliseconds();
+n = x.getMinutes();
+n = x.getMonth();
+n = x.getSeconds();
+n = x.getTime();
+n = x.getTimezoneOffset();
+n = x.getUTCDate();
+n = x.getUTCDay();
+n = x.getUTCFullYear();
+n = x.getUTCHours();
+n = x.getUTCMilliseconds();
+n = x.getUTCMinutes();
+n = x.getUTCMonth();
+n = x.getUTCSeconds();
