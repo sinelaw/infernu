3e7579327a34ba82d37ba45be78b885e184d7f83
Noam Lewis <jones.noamle@gmail.com>
2015-06-09 23:14:12 +0300

    add Math object basic support

--- src/Infernu/Builtins/Operators.hs
+++ src/Infernu/Builtins/Operators.hs
@@ -28,6 +28,11 @@ numRelation = binaryFuncS number number boolean
 numOp :: TypeScheme
 numOp = binaryFuncS number number number
 
+numProp name = TRowProp (TPropName name) $ ts [] $ number
+
+numFuncProp name = TRowProp (TPropName name) $ ts [] $ Fix $ TFunc [tvar 0, number] number
+numFuncProp2 name = TRowProp (TPropName name) $ ts [] $ Fix $ TFunc [tvar 0, number, number] number
+
 builtins :: Map EVarName TypeScheme
 builtins = Map.fromList [
   ("!",            unaryFunc boolean boolean),
@@ -77,5 +82,53 @@ builtins = Map.fromList [
                                 $ TRowProp (TPropName "now")   (ts [] $ Fix $ TFunc [tvar 0] (Fix $ TBody TDate))
                                 $ TRowProp (TPropName "parse") (ts [] $ Fix $ TFunc [tvar 0, string] (Fix $ TBody TDate))
                                 $ TRowProp (TPropName "UTC")   (ts [] $ Fix $ TFunc [tvar 0, number, number, number, number, number, number, number] (Fix $ TBody TDate))
+                                $ TRowEnd Nothing),
+  ("Math",         ts [0] $ Fix $ TRow (Just "Math")
+                                $ numProp "E"
+                                $ numProp "LN10"
+                                $ numProp "LN2"
+                                $ numProp "LOG10E"
+                                $ numProp "LOG2E"
+                                $ numProp "PI"
+                                $ numProp "SQRT1_2"
+                                $ numProp "SQRT2"
+                                $ numFuncProp "abs"
+                                $ numFuncProp "acos"
+   --                             $ numFuncProp "acosh"
+                                $ numFuncProp "asin"
+     --                           $ numFuncProp "asinh"
+                                $ numFuncProp "atan"
+                                $ numFuncProp2 "atan2"
+ --                               $ numFuncProp "atanh"
+ --                               $ numFuncProp "cbrt"
+                                $ numFuncProp "ceil"
+ --                               $ numFuncProp "clz32"
+                                $ numFuncProp "cos"
+ --                               $ numFuncProp "cosh"
+                                $ numFuncProp "exp"
+ --                               $ numFuncProp "expm1"
+                                $ numFuncProp "floor"
+                                --   $ numFuncProp "fround"
+                                --   $ numFuncProp "hypot"
+                                --   $ numFuncProp "imul"
+                                $ numFuncProp "log"
+                                --   $ numFuncProp "log10"
+                                --   $ numFuncProp "log1p"
+                                --   $ numFuncProp "log2"
+
+                                -- Requires support for variable number of arguments
+                                --   $ numFuncProp "max"
+                                --   $ numFuncProp "min"
+
+                                $ numFuncProp2 "pow"
+                                $ TRowProp (TPropName "random") (ts [] $ Fix $ TFunc [tvar 0] number)
+                                $ numFuncProp "round"
+                                $ numFuncProp "sign"
+                                $ numFuncProp "sin"
+                                --   $ numFuncProp "sinh"
+                                $ numFuncProp "sqrt"
+                                $ numFuncProp "tan"
+                                --   $ numFuncProp "tanh"
+                                --   $ numFuncProp "trunc"
                                 $ TRowEnd Nothing)
   ]
---
+++ test/valid/builtins/Math.js
@@ -0,0 +1,29 @@
+var n = 0;
+n = Math.E;
+n = Math.LN10;
+n = Math.LN2;
+n = Math.LOG10E;
+n = Math.LOG2E;
+n = Math.PI;
+n = Math.SQRT1_2;
+n = Math.SQRT2;
+
+n = Math.abs(1);
+n = Math.acos(1);
+n = Math.asin(1);
+n = Math.atan(1);
+n = Math.atan2(1,2);
+n = Math.ceil(1);
+n = Math.cos(1);
+n = Math.exp(1);
+n = Math.floor(1);
+n = Math.log(1);
+n = Math.pow(2,3);
+n = Math.random();
+n = Math.round(1);
+n = Math.sign(1);
+n = Math.sin(1);
+n = Math.sqrt(1);
+n = Math.tan(1);
+
+
