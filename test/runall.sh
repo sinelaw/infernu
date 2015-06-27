fe0e7f1d5cad3e4442a4b7cc3b7ad4888f3608fc
Noam Lewis <jones.noamle@gmail.com>
2015-06-28 00:21:25 +0300

    improve test scripts - don't fail on badly formatted quotes in output

--- test/runall.sh
+++ test/runall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 set -u
-find valid -type f -iname "*.js" | sort |xargs -n1  ./runtest.sh y
-find invalid -type f -iname "*.js" | sort |xargs -n1  ./runtest.sh n
+find valid -type f -iname "*.js" -print0 | xargs -0 -n1  ./runtest.sh y | sort
+find invalid -type f -iname "*.js" -print0 | xargs -0 -n1  ./runtest.sh n | sort
 >&2 echo ""
 
