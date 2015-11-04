89d3a5203b28a527a122daede195d495675e3a0e
Noam Lewis <jones.noamle@gmail.com>
2015-11-04 02:59:00 +0200

    dump functor/applicative/alternative in favor of opaque

--- src/Infernu/Parse/Parser.hs
+++ src/Infernu/Parse/Parser.hs
@@ -2,15 +2,12 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StandaloneDeriving #-}
--- | A simple applicative parser
-
 {-# LANGUAGE NoImplicitPrelude #-}
+
+-- | A simple applicative parser
 module Infernu.Parse.Parser where
 
-import           Control.Applicative (Alternative(..), (<|>))
-import           Data.List (intercalate)
-
-
+import Infernu.Decycle (decycle2)
 import           Data.Monoid ((<>), mconcat)
 import qualified Data.Char as Char
 
@@ -21,155 +18,111 @@ type Stream a = [a]
 emptyStream :: [a]
 emptyStream = []
 
-data ParserSingle a t = ParserSingle (Stream a -> Maybe (Stream a, t))
+data PElem a = PElem [a] -- list of matching characters
+             deriving (Eq, Ord, Show)
 
-unParserSingle :: ParserSingle s a -> Stream s -> Maybe (Stream s, a)
-unParserSingle (ParserSingle p) = p
+-- runPElem :: Eq s => PElem s -> Stream s -> Maybe (Stream s, s)
+runPElem _ [] = Nothing
+runPElem (PElem cs) (s:ss)
+    | s `elem` cs = Just (ss, s)
+    | otherwise   = Nothing
 
-runParserSingle :: ParserSingle s a -> Stream s -> Maybe (Stream s, a)
-runParserSingle (ParserSingle p) s = p s
+class Opaque f where
+    infixl ^|
+    (^|) :: f a -> f a -> f a
+    none :: f a
 
-instance Functor (ParserSingle s) where
-    fmap f (ParserSingle p) = ParserSingle $ \s ->
-        {-# SCC "ParserSingle_fmap" #-}
-        case p s of
-        Nothing -> Nothing
-        Just (s', t) -> Just (s', f t)
+data Parser a where
+    PZero :: Parser a
+    POneOf  :: (Eq a, Show a) => PElem a -> Parser a
+    PAlt  :: [Parser a] -> Parser a
+    POneOrMore :: Parser a -> Parser a
+    PSeq  :: Monoid a => [Parser a] -> Parser a
 
+deriving instance Show (Parser a)
+deriving instance Eq (Parser a)
+deriving instance Ord a => Ord (Parser a)
 
-instance Applicative (ParserSingle s) where
-    pure x = ParserSingle $ \s -> Just (s, x)
-    (ParserSingle pf) <*> ppx =
-        {-# SCC "ParserSingle_<*>" #-}
-        ParserSingle $ \s -> case pf s of
-        Nothing -> Nothing
-        Just (s', f) -> case unParserSingle ppx s' of
-            Nothing -> Nothing
-            Just (s'', x) -> Just (s'', f x)
+instance Opaque Parser where
+    none     = PZero
+    p1 ^| p2 = PAlt [p1, p2]
 
-instance Alternative (ParserSingle s) where
-    empty = ParserSingle $ const Nothing
-    (ParserSingle px) <|> y =
-        {-# SCC "ParserSingle_<|>" #-}
-        ParserSingle $ \s ->
-        case px s of
-        Nothing -> unParserSingle y s
-        Just (s', t) -> Just (s', t)
+oneOrMore p         = POneOrMore p
+zeroOrMore p         = none ^| oneOrMore p
 
-
-data Parser a t where
-    PZero :: Parser a t
-    POne :: (ParserSingle a t) -> Parser a t
-    PAlt :: [Parser a t] -> Parser a t
-    PApp :: (Parser a (u -> t)) -> (Parser a u) -> Parser a t
-    PSome :: Parser a t -> Parser a [t]
-    PSeq :: Monoid t => [Parser a t] -> Parser a t
-
-instance Show (Parser a t) where
-    show (PZero)      = "PZero"
-    show (POne _)     = "POne"
-    show (PAlt ps)    = "(" ++ intercalate " | " (map show ps) ++ ")"
-    show (PApp pf px) = show pf ++ " <*> " ++ show px
-    show (PSome p)    = "some " ++ show p
-    show (PSeq ps)    = "(" ++ intercalate ", " (map show ps) ++ ")"
-
-instance Functor (Parser s) where
-    fmap f p = pure f <*> p
-
-instance Applicative (Parser s) where
-    pure    = POne . pure
-    p <*> x = PApp p x
-
-instance Alternative (Parser s) where
-    empty          = PZero
-    p1      <|> p2 = PAlt [p1, p2]
-
-    some p         = PSome p
-    many p         = some p <|> pure []
-
-instance Monoid a => Monoid (Parser s a) where
+instance Monoid a => Monoid (Parser a) where
     mempty        = PZero
     x `mappend` y = PSeq [y,x]
-
-cons :: Parser s a -> Parser s [a] -> Parser s [a]
-cons x xs = (fmap (:[]) x) <> xs
 
 fmapParserResult :: (a -> b) -> (s, a) -> (s, b)
 fmapParserResult = fmap
 
--- flatten :: [Maybe a] -> Maybe [a]
--- flatten = emptyToNothing . catMaybes
---     where
---         emptyToNothing [] = Nothing
---         emptyToNothing xs = Just xs
+type RunParser a = (Parser a -> Stream a -> [(Stream a, [a])])
 
-runParserSome :: forall a t. Parser a t -> Stream a -> [(Stream a, [t])]
-runParserSome p s = do
-    (s', t) <- runParser p s
-    res <- (s', []) : runParserSome p s'
-    return $ fmapParserResult (t:) res
+runParserSome :: forall a. Eq a => RunParser a -> Parser a -> Stream a -> [(Stream a, [a])]
+runParserSome r p s = do
+    (s', t) <- r p s
+    res <- (s', []) : runParserSome r p s'
+    return $ fmapParserResult (t++) res
 
-runParserAlt :: [Parser a t] -> Stream a -> [(Stream a, t)]
-runParserAlt ps s = concat $ map (flip runParser s) ps
+runParserAlt :: Eq a => RunParser a ->  [Parser a] -> Stream a -> [(Stream a, [a])]
+runParserAlt r ps s = concat $ map (flip r s) ps
 
-runParserSeq :: forall a t. Monoid t => [Parser a t] -> Stream a -> [(Stream a, t)]
-runParserSeq ps stream = foldr flattenSeq [(stream, mempty)] ps
+runParserSeq :: forall a t. (Eq a, Monoid a) => RunParser a -> [Parser a] -> Stream a -> [(Stream a, [a])]
+runParserSeq r ps stream = foldr flattenSeq [(stream, mempty)] ps
     where
-        flattenSeq :: Parser a t -> [(Stream a, t)] -> [(Stream a, t)]
+        -- flattenSeq :: Parser a -> [(Stream a, a)] -> [(Stream a, a)]
         flattenSeq p alts = do
             (s, t) <- alts
-            (s', t') <- runParser p s
+            (s', t') <- r p s
             return (s', t <> t')
 
-runParser :: forall a t. Parser a t -> Stream a -> [(Stream a, t)]
-runParser PZero _ = []
-runParser (POne p) s =
-    case runParserSingle p s of
+runParserOneOf :: Eq a => PElem a -> Stream a -> [(Stream a, [a])]
+runParserOneOf p s =
+    case runPElem p s of
     Nothing -> []
-    Just (s', t) -> [(s', t)]
-runParser (PApp pf px) s = do
-    (s1, f) <- runParser pf s
-    (s2, x) <- runParser px s1
-    return (s2, f x)
-runParser (PSome p) s = runParserSome p s
-runParser (PAlt ps) s = runParserAlt ps s
-runParser (PSeq ps) s = runParserSeq ps s
+    Just (s', t) -> [(s', [t])]
+
+runParser'' :: forall a. Eq a => RunParser a -> Parser a -> Stream a -> [(Stream a, [a])]
+runParser'' r  PZero _ = []
+runParser'' r (POneOf p) s = runParserOneOf p s
+runParser'' r (POneOrMore p) s = runParserSome r p s
+runParser'' r (PAlt ps) s = runParserAlt r ps s
+runParser'' r (PSeq ps) s = runParserSeq r ps s
+
+runParser' Nothing _ _ = []
+runParser' (Just r) p s = runParser'' r p s
+
+runParser :: Ord a => RunParser a
+runParser = decycle2 runParser'
 
 ----------------------------------------------------------------------
 
-isSingle :: (t -> Bool) -> ParserSingle t t
-isSingle f = ParserSingle $
-           \s -> case s of
-           (x:s') | f x -> Just (s', x)
-           _ -> Nothing
+is :: (Eq t, Show t) => [t] -> Parser t
+is = POneOf . PElem
 
-is :: (t -> Bool) -> Parser t t
-is = POne . isSingle
+are :: (Eq a, Show a) => [a] -> Parser a
+are = zeroOrMore . is
 
-are :: (a -> Bool) -> Parser a [a]
-are = many . is
+allChars = ['a'..'z'] ++ ['0'..'9'] ++ ['A'..'Z']
+isChar f = is $ filter f allChars
+lower = isChar Char.isLower
+digit = isChar Char.isDigit
+upper = isChar Char.isUpper
+letter = isChar Char.isLetter
+alphaNum = isChar Char.isAlphaNum
 
-oneOf opts = is (\x -> elem x opts)
+str = oneOrMore letter
 
-lower = is Char.isLower
-upper = is Char.isUpper
-letter = is Char.isLetter
-alphaNum = is Char.isAlphaNum
+space = isChar Char.isSpace
 
-x `followedBy` y = (,) <$> x <*> y
+openPar = is ['(']
+closePar = is [')']
 
+-- withParens :: Parser Char a -> Parser Char a
+-- withParens x = openPar *> x <* closePar
 
-str = some letter
-
-space = is Char.isSpace
-
-openPar = is (=='(')
-closePar = is (==')')
-
-withParens :: Parser Char a -> Parser Char a
-withParens x = openPar *> x <* closePar
-
-optParens :: Parser Char a -> Parser Char a
-optParens x = x <|> withParens x
+-- optParens :: Parser Char a -> Parser Char a
+-- optParens x = x ^| withParens x
 
 
